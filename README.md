726aacb385c61da338883b569c6a837e3f5a5103
allansouzadk <allan.pereira.araujo02@gmail.com>
2022-02-08 01:26:40 -0300

    discord bot template

---
+++ README.md
@@ -0,0 +1,14 @@
+## Discord bot template using nodejs
+
+## Start 
+
+```set in .env BOT_TOKEN , CLIENT_ID, GUILD_ID , follow the example left```
+``` npm start ```
+
+## Author
+
+[**Allan Pereira**](https://github.com/allansouzadk)
+
+## License
+
+none
---
+++ src/bot.js
@@ -0,0 +1,44 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv');
+dotenv.config();
+const TOKEN = process.env.BOT_TOKEN;
+
+const commands = [{
+  name: 'ping',
+  description: 'Replies with Pong!'
+}];
+
+const rest = new REST({ version: '9' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.login(TOKEN);
